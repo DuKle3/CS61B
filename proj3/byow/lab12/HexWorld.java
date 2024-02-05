c973e66dbcb41fbe8ceffc0fe6ee59be77138bca
kenny <kennylee1112@gmail.com>
2024-02-05 09:03:35 +0800

    lab12 Complete

--- proj3/byow/lab12/HexWorld.java
+++ proj3/byow/lab12/HexWorld.java
@@ -1,6 +1,4 @@
 package byow.lab12;
-import org.junit.Test;
-import static org.junit.Assert.*;
 
 import byow.TileEngine.TERenderer;
 import byow.TileEngine.TETile;
@@ -12,4 +10,140 @@ import java.util.Random;
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+    private static final int WIDTH = 50;
+    private static final int HEIGHT = 50;
+    private static final long SEED = 2873123;
+    private static final Random RANDOM = new Random(SEED);
+
+    private static void fillBoardWithNothing(TETile[][] tiles) {
+        int height = tiles[0].length;
+        int width = tiles.length;
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                tiles[x][y] = Tileset.NOTHING;
+            }
+        }
+    }
+
+    /**
+     * Draw a row of tiles to the board start with given Position.
+     */
+    public static void drawRow(TETile[][] tiles, Position p, TETile type, int length) {
+        for (int dx = 0; dx < length; dx ++) {
+            tiles[p.x + dx][p.y] = type;
+        }
+    }
+
+    /**
+     * Adds a hexagon to the world at position P of size size.
+     */
+
+    public static void addHexagon(TETile[][] tiles, Position p, TETile type, int size) {
+        if (size < 2) {
+            return;
+        }
+        addHexagonHelper(tiles, p, type, size, size - 1, size);
+    }
+
+    /**
+     * Helper method for addHexagon.
+     */
+    private static void addHexagonHelper(TETile[][] tiles, Position p, TETile type, int n, int blank, int t) {
+        // Draw this row
+        Position startOfRow = p.shift(blank, 0);
+        drawRow(tiles, startOfRow, type, t);
+
+        // Draw remaining rows recursively
+        if (blank > 0) {
+            Position nextOfRow = p.shift(0, -1);
+            addHexagonHelper(tiles, nextOfRow, type, n - 1, blank - 1, t + 2);
+        }
+
+        // Draw this row again to be the reflection
+        Position startOfReflectedRow = startOfRow.shift(0, -(2 * n - 1));
+        drawRow(tiles, startOfReflectedRow, type, t);
+    }
+
+    private static TETile randomTile() {
+        int tileNum = RANDOM.nextInt(5);
+        switch (tileNum) {
+            case 0: return Tileset.GRASS;
+            case 1: return Tileset.FLOWER;
+            case 2: return Tileset.SAND;
+            case 3: return Tileset.MOUNTAIN;
+            case 4: return Tileset.TREE;
+            default: return Tileset.GRASS;
+        }
+    }
+    /**
+     * Draw a column of Hexagon with int size and int num to the world at Position p with TETile type.
+     */
+    public static void addHexCol(TETile[][] tiles, Position p, int size, int num) {
+        // Draw this Hexagon
+        addHexagon(tiles, p, randomTile(), size);
+        // Draw other Hexagon below it
+        if (num > 1) {
+            Position bottomNeighbor = p.getBottomNeighbor(size);
+            addHexCol(tiles, bottomNeighbor, size, num - 1);
+        }
+    }
+
+
+    private static class Position {
+        int x;
+        int y;
+
+        Position(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Position shift(int dx, int dy) {
+            return new Position(this.x + dx, this.y + dy);
+        }
+
+        public Position getBottomNeighbor(int hexSize) {
+            return new Position(this.x, this.y - 2*hexSize);
+        }
+
+        public Position getTopRightNeighbor(int hexSize) {
+            return new Position(this.x + 2*hexSize - 1, this.y + hexSize);
+        }
+
+        public Position getBottomRightNeighbor(int hexSize) {
+            return new Position(this.x + 2*hexSize - 1, this.y - hexSize);
+        }
+    }
+
+    /**
+     * Draws the hexagonal world
+     */
+    public static void drawWorld(TETile[][] tiles, Position p, int hexSize, int tessSize) {
+
+        addHexCol(tiles, p, hexSize, tessSize);
+
+        // Draw the right and up hexagon
+        for (int i = 1; i < tessSize; i++) {
+            p = p.getTopRightNeighbor(hexSize);
+            addHexCol(tiles, p, hexSize, tessSize + i);
+        }
+        // Draw the right and down hexagon
+        for (int i = 1; i < tessSize; i++) {
+            p = p.getBottomRightNeighbor(hexSize);
+            addHexCol(tiles, p, hexSize, 2*tessSize - 1 - i);
+        }
+    }
+
+    public static void main(String[] args) {
+        TERenderer ter = new TERenderer();
+        ter.initialize(WIDTH, HEIGHT);
+
+        TETile[][] hexWorld = new TETile[WIDTH][HEIGHT];
+        fillBoardWithNothing(hexWorld);
+
+        Position p = new Position(5, 35);
+        drawWorld(hexWorld, p, 4, 3);
+
+        ter.renderFrame(hexWorld);
+    }
 }
--- proj3/byow/lab12/project3prep.md
+++ proj3/byow/lab12/project3prep.md
@@ -4,22 +4,22 @@
 After looking at your own implementation, consider the implementation provided near the end of the lab.
 How did your implementation differ from the given one? What lessons can be learned from it?**
 
-Answer:
+Answer: My first implementation of hexagon is less hierarchical structure than the TA's live code, and more tactical coding style.
 
 -----
 
 **Can you think of an analogy between the process of tessellating hexagons and randomly generating a world using rooms and hallways?
 What is the hexagon and what is the tesselation on the Project 3 side?**
 
-Answer:
+Answer: the column of hexagon which I use recursion to build which I think is like the wall to Project 3.
 
 -----
 **If you were to start working on world generation, what kind of method would you think of writing first? 
 Think back to the lab and the process used to eventually get to tessellating hexagons.**
 
-Answer:
+Answer: I would like to write the route method.
 
 -----
 **What distinguishes a hallway from a room? How are they similar?**
 
-Answer:
+Answer: hallway have only one width, and a room are rectangle.
